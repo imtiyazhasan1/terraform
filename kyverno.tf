5d4587993bda7ad6fa80baacdf1d12d83b84e353
bhagyashri.bahirat@vodafone.com <bhagyashri.bahirat@vodafone.com>
2021-11-19 19:01:03 +0530

    kyverno.tf
    added version

--- kyverno.tf
+++ kyverno.tf
@@ -4,6 +4,7 @@ resource "helm_release" "k8sPolicyCRDs" {
   repository = "https://registry.eu-central-1.harbor.vodafone.com/chartrepo/gks-public-cloud"
   repository_username = lookup(var.Harbor_creds,"username")
   repository_password = lookup(var.Harbor_creds,"password")
+  version    = "v2.0.2"
   depends_on = [
     aws_eks_node_group.eks-cluster-workerNodeGroup,
 	  aws_route53_record.harbor-ns,kubernetes_secret.kyverno-docker-secret
@@ -22,6 +23,7 @@ resource "helm_release" "kyverno" {
   repository = "https://registry.eu-central-1.harbor.vodafone.com/chartrepo/gks-public-cloud"
   repository_username = lookup(var.Harbor_creds,"username")
   repository_password = lookup(var.Harbor_creds,"password")
+  version    = "v2.0.2"
   chart      = "kyverno"
   timeout = 300
   set {
